203b182ebea27553aa57da26c45a6bc1f8a4d13b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-03-27 10:19:31 +0000

    Set up prisma and session schema

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # odin-nodejs-prisma-orm
+
 https://www.theodinproject.com/lessons/nodejs-prisma-orm
--- app.mjs
+++ app.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req, res, next) => {
-  res.send('Testing!');
+app.get('/', async (req, res, next) => {
+  const sessions = await prisma.session.findMany();
+  res.send(sessions);
 });
 
 app.listen(8080, () => console.log('Listening on port', 8080));
