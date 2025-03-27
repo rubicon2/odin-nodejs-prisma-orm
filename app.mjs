b82e2e164e308b6f98fab9032853ef0815e4786f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-03-27 10:32:10 +0000

    Set up express session

--- app.mjs
+++ app.mjs
@@ -1,12 +1,24 @@
 import express from 'express';
+import expressSession from 'express-session';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
+app.use(
+  expressSession({
+    secret: 'who cares',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      // 7 days.
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  }),
+);
 const prisma = new PrismaClient();
 
 app.get('/', async (req, res, next) => {
-  const sessions = await prisma.session.findMany();
-  res.send(sessions);
+  const session = req.session;
+  res.send(session);
 });
 
 app.listen(8080, () => console.log('Listening on port', 8080));
